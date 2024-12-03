e17b40b16850d7b6181ceb0cd77858e29048a24a
SubashThulung <subashwhive@gmail.com>
2024-12-03 21:07:26 +0545

    helper update

--- app/Helper.php
+++ app/Helper.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\DB;
 function getImage($folder, $file)
 {
 
-    return "http://localhost:8002/uploads/$folder/$file";
-    // return "https://advisornp.com/AdminBoard/uploads/$folder/$file";
+    // return "http://localhost:8002/uploads/$folder/$file";
+    return "https://orchidindependentcaring.com.au/AdminBoard/uploads/$folder/$file";
 }
 
 function siteSetting()
