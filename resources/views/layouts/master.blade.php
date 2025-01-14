4f763c3151417a2069cca337796485f8fd5ac2d9
RaiSubash <subashthulung13@gmail.com>
2025-01-14 22:24:11 +0545

    master blade updated

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,11 +5,10 @@
 <head>
     <meta charset="utf-8" />
     <meta http-equiv="x-ua-compatible" content="ie=edge" />
-    <title>Orchid Indendent Caring</title>
+    <title>{{siteSetting()->site_name}}</title>
     <meta name="description"
         content="Orchid Independent caring Pty Ltd acknowledge the Traditional owners and custodians of Country throughout Australia and acknowledges their continuing connection to land, sea and community. We pay our respects to the Elders past, present and emerging." />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    {{-- <link rel="apple-touch-icon" href="{{ getImage('site', siteSetting()->favicon) }}" /> --}}
     <link rel="shortcut icon" href="{{ getImage('site', siteSetting()->favicon) }}" />
     <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700" rel="stylesheet" />
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,400i,500,600,700" rel="stylesheet" />
