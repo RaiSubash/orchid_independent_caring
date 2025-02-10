94f63e31e6e9336e1c811d8451f1702630ee158d
RaiSubash <subashthulung13@gmail.com>
2025-02-10 07:34:41 +0545

    Helper updated

--- app/Helper.php
+++ app/Helper.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\DB;
 function getImage($folder, $file)
 {
 
-    // return "http://localhost:8002/uploads/$folder/$file";
-    return "https://orchidindependentcaring.com.au/AdminBoard/uploads/$folder/$file";
+    return "http://localhost:8002/uploads/$folder/$file";
+    // return "https://orchidindependentcaring.com.au/AdminBoard/uploads/$folder/$file";
 }
 
 function siteSetting()
@@ -22,5 +22,6 @@ function services()
 function about()
 {
     return DB::table('about_us')->where('status', 1)
+        ->where('show_in_about', 1)
         ->get();
 }
